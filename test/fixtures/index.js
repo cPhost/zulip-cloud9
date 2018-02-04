08a7ae7d53d2dcdd1cad0e630b074d81b08f25a3
Priyank Patel <priyank3219@gmail.com>
2018-02-04 18:12:58 +0000

    tests: Addd start-services test.

---
+++ test/fixtures/index.js
@@ -0,0 +1,61 @@
+const { spawnSync } = require('child_process');
+const SERVICES = [
+  'memcached',
+  'rabbitmq-server',
+  'redis-server',
+  'postgresql'
+];
+
+function runSync(cmd, args, opts = {}) {
+  let proc = spawnSync(cmd, args, Object.assign({
+    cwd: process.cwd()
+  }, opts));
+  return proc;
+}
+
+function isServiceStarted(service) {
+  let status = runSync('sudo', ['service', service, 'status']);
+  status = status.stdout.toString();
+  let started = true;
+
+  console.log(status);
+  switch (service) {
+    case 'memcached':
+      started = !status.includes('not running');
+      break;
+
+    case 'rabbitmq-server':
+      started = !/diagnostics/gi.test(status);
+      break;
+
+    case 'redis-server':
+      started = !status.includes('not running');
+      break;
+
+    case 'postgresql':
+      started = !status.includes('down');
+      break;
+
+    default:
+      // no services matched so it's a typo
+      // somewhere faile to let user know
+      const msg = `Invalid service passed to checkServicesStarted: ${service}`;
+      throw new Error(msg);
+  }
+
+  return started;
+}
+
+function checkServicesStarted(service) {
+  const services = service ? [service] : SERVICES;
+  const status = services.every(isServiceStarted);
+  if (!status) {
+    return new Error('All services are not started.');
+  }
+}
+
+module.exports = {
+  runSync,
+  SERVICES,
+  checkServicesStarted
+};
---
+++ test/unit/start-services.test.js
@@ -0,0 +1,64 @@
+const fixtures = require('../fixtures');
+const startServices = require('../../lib/start-services');
+
+const {
+  runSync, checkServicesStarted, SERVICES
+} = fixtures;
+
+describe('start-services start', () => {
+  it('should start all the services', function(done) {
+    startServices()
+      .then(() => {
+        const res = checkServicesStarted();
+        done(res);
+      })
+      .catch((err) => {
+        done(err);
+      });
+  });
+
+  describe('test starts all services if stopped', () => {
+    const services = SERVICES;
+    // remove memcached from test list since if you stop it manually
+    // it will crash
+    services.splice(services.indexOf('memcached'), 1);
+
+    beforeEach(function() {
+      if (!services.length === 3) { services.splice(0, 1); }
+      runSync('sudo', ['service', services[0], 'stop']);
+    });
+
+    it('starts rabbitmq-server if its stopped', function(done) {
+      startServices()
+        .then(() => {
+          const res = checkServicesStarted('memcached');
+          done(res);
+        })
+        .catch((err) => {
+          done(err);
+        });
+    });
+
+    it('starts redis-server if its stopped', function(done) {
+      startServices()
+        .then(() => {
+          const res = checkServicesStarted('redis-server');
+          done(res);
+        })
+        .catch((err) => {
+          done(err);
+        });
+    });
+
+    it('starts postgresql if its stopped', function(done) {
+      startServices()
+        .then(() => {
+          const res = checkServicesStarted('postgresql');
+          done(res);
+        })
+        .catch((err) => {
+          done(err);
+        });
+    });
+  });
+});
