df0197b74afbfc35155ef3e8f3e0c80c03c40a37
Priyank Patel <priyank3219@gmail.com>
2018-02-11 02:58:20 +0000

    tests: Increase code coverage of utils.js.

---
+++ test/unit/utils.test.js
@@ -0,0 +1,45 @@
+const utils = require('../../lib/utils');
+const assert = require('assert');
+const mockFS = require('mock-fs');
+const sinon = require('sinon');
+
+describe('utils test', () => {
+  describe('defaultZulipPath', () => {
+    const exitStub = sinon.stub(process, 'exit');
+    after(() => {
+      exitStub.restore();
+    });
+
+    it('should fail if default path does not exsist', () => {
+      mockFS({});
+      utils.parseZulipPath();
+      assert(exitStub.calledOnce);
+      mockFS.restore();
+    });
+
+    it('should return process.cwd is it\'s a zulip directory', () => {
+      const expected = '/zulip-cloud9/zulip';
+      const cwdStub = sinon.stub(process, 'cwd').returns(expected);
+
+      const actual = utils.parseZulipPath();
+      assert.deepStrictEqual(expected, actual);
+      cwdStub.restore();
+    });
+
+    it('should fail if wrong path is passed', () => {
+      utils.parseZulipPath('NON_EXSISTENT/PATH');
+      assert(exitStub.calledTwice);
+    });
+
+    it('should return passed path is it exsists', () => {
+      mockFS({
+        '/home/zulip-cloud9-user/zulip': {}
+      });
+
+      const expected = '/home/zulip-cloud9-user/zulip';
+      const actual = utils.parseZulipPath(expected);
+      assert.deepStrictEqual(expected, actual);
+      mockFS.restore();
+    });
+  });
+});
